8b8a8614379a28142f8e0aac35243ac67cc9a822
Juliano <julianopaulo.santos@hotmail.com>
2021-06-01 08:07:14 -0300

    project start

---
+++ src/pages/routes.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Home from './Home';
+import Mylinks from './MyLinks';
+
+import { Ionicons } from '@expo/vector-icons';
+
+const Drawer = createDrawerNavigator();
+
+export default function Routes(){
+    return (
+        <Drawer.Navigator
+            drawerContentOptions={{
+                activeBackgroundColor: '#2ccbb9',
+                activeTintColor: '#FFF',
+                marginTop: 16,
+                labelStyle:{
+                    fontSize: 19
+                }
+            }}
+        >
+
+            <Drawer.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    title: 'Encurtar Link',
+                    drawerIcon: ({ focused, size, color}) => (
+                        <Ionicons
+                            name={focused? 'cube':'cube-outline'}
+                            color={color}
+                            size={size}
+                        />
+                    )
+                }}
+            />
+
+            <Drawer.Screen
+                name="MyLinks"
+                component={Mylinks}
+                options={{
+                    title: 'Meus Links',
+                    drawerIcon: ({ focused, size, color}) => (
+                        <Ionicons
+                            name={focused? 'stats-chart':'stats-chart-outline'}
+                            color={color}
+                            size={size}
+                        />
+                    )
+                }}
+            />
+
+        </Drawer.Navigator>
+    )
+}
